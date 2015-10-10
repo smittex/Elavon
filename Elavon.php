1051b24f7a928808175ccb3d616ed60c13d3e572
Bryan Smith <ext.bsmith@hexiscyber.com>
2015-10-10 13:24:51 -0500

    Cleaning up

--- Elavon.php
+++ Elavon.php
@@ -12,6 +12,12 @@ License: GPL2
 
 function show_processing_form($atts)
 {
+    // TODO: Assuming email is being passed via GET parameter and forwarded to Elavon as the key for the transaction
+    // TODO: This COULD be dangerous!
+
+    // TODO: Also assuming the VT is configured to disable changing of email address
+
+    // TODO: Make this configurable
     //Uncomment the endpoint desired.
     //Production URL
     //$url ='https://www.myvirtualmerchant.com/VirtualMerchant/process.do';
@@ -19,14 +25,22 @@ function show_processing_form($atts)
     $url = 'https://demo.myvirtualmerchant.com/VirtualMerchantDemo/process.do';
 
     //Configuration parameters.
+    // TODO: Put merchant_id, user_id, and pin in a configuration form
     $ssl_merchant_id            = 'xxxxxx';
     $ssl_user_id                = 'xxxxxx';
     $ssl_pin                    = 'xxxxxx';
+
+    // TODO: Make receipt_link_url configurable
+    $ssl_receipt_link_url       = 'your_receipt_page_url_here';
+
+    //TODO: Make amount configurable; This could be passed in via a GET parameter but it's VERY dangerous!
+    $ssl_amount                 = '35.00';
+
+    // No plans to make these configurable
     $ssl_show_form              = 'true';
     $ssl_result_format          = 'HTML';
     $ssl_test_mode              = 'false';
     $ssl_receipt_link_method    = 'REDG';
-    $ssl_receipt_link_url       = 'your_receipt_page_url_here';
     $ssl_transaction_type       = 'CCSALE';
     $ssl_cvv2cvc2_indicator     = '1';
 
@@ -35,7 +49,12 @@ function show_processing_form($atts)
         echo '<html><head><base href="' . $url . '"></base></head>';
     }
 
-    //Dynamically builds POST request based on the information being passed into the script.
+    /*
+     * Dynamically builds POST request based on the information being passed into the script.
+     *
+     * TODO: This COULD be dangerous!
+     * TODO: Filter what GET parameters may be passed on to Elavon
+     */
     $queryString = '';
 
     foreach ($_REQUEST as $key => $value) {
@@ -55,6 +74,7 @@ function show_processing_form($atts)
         "&ssl_pin=$ssl_pin" .
 
         "&ssl_transaction_type=$ssl_transaction_type" .
+        "&ssl_amount=$ssl_amount" .
 
         "&ssl_cvv2cvc2_indicator=$ssl_cvv2cvc2_indicator" .
         "&ssl_show_form=$ssl_show_form" .
